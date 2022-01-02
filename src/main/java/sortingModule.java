fee1cb0de002549240735219b52eed1efe5b19df
CianFitzgerald <cian.fitzgerald97@gmail.com>
2022-01-02 20:25:04 +0000

    changes

--- src/main/java/sortingModule.java
+++ src/main/java/sortingModule.java
@@ -6,8 +6,8 @@ public class sortingModule extends Module {
     public String sort(int forward, int left, int right, int back) {
         int lowest = Math.min(Math.min(forward, left), Math.min(right, back));
 
-        if (lowest == back) {
-			return "back";
+        if (lowest == forward) {
+			return "forward";
 		}
 		else if (lowest == left) {
 			return "left";
@@ -16,8 +16,9 @@ public class sortingModule extends Module {
 			return "right";
 		}
 		else  {
-			return "forward";
+			return "back";
 		}
+
 
     }
 }

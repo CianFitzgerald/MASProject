568dcb8da48952c1dc763e4c29261c7fba00dc24
CianFitzgerald <cian.fitzgerald97@gmail.com>
2022-01-03 14:57:22 +0000

    refactor and obstacle stuff

--- src/main/java/arrayModule.java
+++ src/main/java/arrayModule.java
@@ -10,7 +10,7 @@ public class arrayModule extends Module {
             int x = array[xcord][ycord];
             return x;
         } catch (Exception e) {
-            int x = 10000;
+            int x = 1000;
             return x;
         } 
     }
@@ -20,5 +20,69 @@ public class arrayModule extends Module {
         array[xcord][ycord] += 1;
         return true;
     }
+
+    @ACTION
+    public boolean addObstacle(int xcord, int ycord){
+        try{
+            array[xcord][ycord] += 1000;
+            return true;
+        } catch (Exception e){
+            return true;
+        }
+    }
+
+    @ACTION
+    public boolean ObstacleCheck(String dir, int xcord, int ycord, String square, String contents){
+        if(dir=="north"){
+            if(square=="forward" & contents=="obstacle"){
+                addObstacle(xcord, ycord -1);
+            }if(square=="left" & contents=="obstacle"){
+                addObstacle(xcord+1, ycord);
+            }if(square=="right" & contents=="obstacle"){
+                addObstacle(xcord-1, ycord);
+            }if(square=="back" & contents=="obstacle"){
+                addObstacle(xcord, ycord +1);
+            }
+        }
+
+        if(dir=="south"){
+            if(square=="forward" & contents=="obstacle"){
+                addObstacle(xcord, ycord +1);
+            }if(square=="left" & contents=="obstacle"){
+                addObstacle(xcord-1, ycord);
+            }if(square=="right" & contents=="obstacle"){
+                addObstacle(xcord+1, ycord);
+            }if(square=="back" & contents=="obstacle"){
+                addObstacle(xcord, ycord -1);
+            }
+        }
+
+        if(dir=="east"){
+            if(square=="forward" & contents=="obstacle"){
+                addObstacle(xcord+1, ycord);
+            }if(square=="left" & contents=="obstacle"){
+                addObstacle(xcord, ycord-1);
+            }if(square=="right" & contents=="obstacle"){
+                addObstacle(xcord, ycord+1);
+            }if(square=="back" & contents=="obstacle"){
+                addObstacle(xcord-1, ycord);
+            }
+        }
+
+        if(dir=="west"){
+            if(square=="forward" & contents=="obstacle"){
+                addObstacle(xcord-1, ycord);
+            }if(square=="left" & contents=="obstacle"){
+                addObstacle(xcord, ycord+1);
+            }if(square=="right" & contents=="obstacle"){
+                addObstacle(xcord, ycord-1);
+            }if(square=="back" & contents=="obstacle"){
+                addObstacle(xcord+1, ycord);
+            }
+        }
+
+        return true;
+    }
+
 }
 
